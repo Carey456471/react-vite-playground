d2157c2a551c28483647df33452b61298b1decaf
FungTszKai <dev_user33@profield.jp>
2023-11-28 17:36:19 +0900

    add routes

---
+++ src/pages/Login.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Login() {
+    return (
+        <div className="w-screen h-screen dark:bg-zinc-700 bg-zinc-100">
+            <div className="dark:bg-zinc-700 bg-zinc-100 w-full h-auto"></div>
+
+            <div className="w-screen h-screen overflow-hidden relative">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" preserveAspectRatio="none" className="w-full h-full ">
+                    <path
+                        fill="#14b8a6"
+                        fillOpacity={0.4}
+                        d="M0,288L0,128L180,128L180,160L360,160L360,224L540,224L540,128L720,128L720,32L900,32L900,128L1080,128L1080,224L1260,224L1260,32L1440,32L1440,320L1260,320L1260,320L1080,320L1080,320L900,320L900,320L720,320L720,320L540,320L540,320L360,320L360,320L180,320L180,320L0,320L0,320Z"></path>
+                </svg>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
---
+++ src/utils/routes.js
@@ -0,0 +1,8 @@
+const baseUrl = "/react-vite-playground";
+
+const routes = {
+    "login" : baseUrl + "/login",
+    "home" : baseUrl + "/home",
+}
+
+export {routes}
